fcbee11ce6d3a5ce423a8c27ee4998ed3da57041
ZAVÉRA <zaveracoid@gmail.com>
2026-08-17 04:49:26 +0700

    Fix product image paths

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ import Navbar from "../components/Navbar";
 
 const heroSlides = [
   {
-    image: "/images/products/nerea-porcelain/FTO09606.JPG",
+    image: "/images/products/nerea-porcelain/FTO09606.jpg",
     color: "PORCELAIN",
   },
   {
-    image: "/images/products/nerea-truffle/FTO09640.JPG",
+    image: "/images/products/nerea-truffle/FTO09640.jpg",
     color: "TRUFFLE",
   },
   {
-    image: "/images/products/nerea-powder-blue/FTO09686.JPG",
+    image: "/images/products/nerea-powder-blue/FTO09686.jpg",
     color: "POWDER BLUE",
   },
 ];
@@ -23,17 +23,17 @@ const collections = [
   {
     name: "Neréa Blouse",
     color: "Porcelain",
-    image: "/images/products/nerea-porcelain/FTO09606.JPG",
+    image: "/images/products/nerea-porcelain/FTO09606.jpg",
   },
   {
     name: "Neréa Blouse",
     color: "Powder Blue",
-    image: "/images/products/nerea-powder-blue/FTO09686.JPG",
+    image: "/images/products/nerea-powder-blue/FTO09686.jpg",
   },
   {
     name: "Neréa Blouse",
     color: "Truffle",
-    image: "/images/products/nerea-truffle/FTO09640.JPG",
+    image: "/images/products/nerea-truffle/FTO09640.jpg",
   },
 ];
 
